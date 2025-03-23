8e8f9d4f343d679db8f233951afef7b3c622588e
yusufmalkocc <yusufmlkc10@gmail.com>
2025-03-23 16:12:51 +0300

    Update HW1-Q3.cpp

--- HW1-Q3.cpp
+++ HW1-Q3.cpp
@@ -2,13 +2,14 @@
 #include <math.h>
 int main()
 {
-    int x , y , z;
+    int x , y ;
+    double z;
     printf("Enter size of the house:");
     scanf("%d",&x);
     printf("Enter the popularity:");
     scanf("%d",&y);
     z= (pow(y,3)+pow(x,2))*10000;
-    printf("House value is:%d",z);
+    printf("House value is:%lf",z);
 
     return 0;
 }
