05847210747a23596d0ae08fe02e23ac1d47eac5
yusufmalkocc <yusufmlkc10@gmail.com>
2025-03-23 16:27:52 +0300

    Add files via upload

---
+++ HW1-Q2.cpp
@@ -0,0 +1,41 @@
+/*
+#include <stdio.h>
+
+
+int main( void )
+{
+   int integer1 
+   int integer2
+   int x;      
+
+   printf( Enter first integer\n );
+   scanf( "%d", &integer1 );        
+
+   printf( "Enter second integer\n" ); 
+   scanf( "%d", &integer2 );           
+   
+   sum = integer1 + integer2; 
+
+   printf( "Sum is %d\n", sum ); 
+   return 0; //
+
+} /* end function main */
+/* Addition program */
+#include <stdio.h>
+
+int main( void )
+{
+   int integer1 , integer2 ,sum ;      
+
+   printf( "Enter first integer\n" ); 
+   scanf( "%d", &integer1 );        
+
+   printf( "Enter second integer\n" );
+   scanf( "%d", &integer2 );          
+   
+   sum = integer1 + integer2; 
+
+   printf( "Sum is %d\n", sum ); 
+   return 0; 
+
+} 
